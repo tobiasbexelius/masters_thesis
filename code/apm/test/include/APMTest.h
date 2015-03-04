a13f7fc33bb9ca3ec4caa9310e2149692a282ec0
Tobias Andersson <tobias.ason@gmail.com>
2015-03-04 17:14:35 +0100

    Dummy implementation

--- code/apm/test/include/APMTest.h
+++ code/apm/test/include/APMTest.h
@@ -5,8 +5,17 @@ namespace automatic_package_measuring {
 
 class APMTest {
 public:
-	APMTest();
+	APMTest(APMTestCase testCase, double max_error = 0.05);
 	virtual ~APMTest();
+	void run();
+	double getError();
+	bool success() const;
+	std::vector<cv::Point2i> getExpectedCorners() const;
+	std::vector<cv::Point2i> getActualCorners() const;
+private:
+	const APMTestCase testCase;
+	const double max_error;
+	std::vector<cv::Point2i> actualCorners;
 };
 
 } /* namespace automatic_package_measuring */
--- code/apm/test/src/APMTest.cpp
+++ code/apm/test/src/APMTest.cpp
@@ -2,11 +2,30 @@
 
 namespace automatic_package_measuring {
 
-APMTest::APMTest() {
-
+APMTest::APMTest(APMTestCase testCase, double max_error) :
+		testCase(testCase), max_error(max_error) {
 }
 
 APMTest::~APMTest() {
+}
+
+void APMTest::run() {
+}
+
+bool APMTest::success() const {
+	return true;
+}
+
+std::vector<cv::Point2i> APMTest::getExpectedCorners() const {
+	return testCase.getCorners();
+}
+
+double APMTest::getError() {
+	return -1.0;
+}
+
+std::vector<cv::Point2i> APMTest::getActualCorners() const {
+	return actualCorners;
 }
 
 } /* namespace automatic_package_measuring */
