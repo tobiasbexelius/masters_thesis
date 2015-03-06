7362d56df1fb6e27dd9c4dfa93f86a590c8558d9
Tobias Andersson <tobias.ason@gmail.com>
2015-03-06 14:18:13 +0100

    Simpify convexity check

--- code/apm/lib/include/PolygonFinder.h
+++ code/apm/lib/include/PolygonFinder.h
@@ -7,7 +7,7 @@ namespace automatic_package_measuring {
 
 class PolygonFinder {
 public:
-	PolygonFinder(double error_tolerance = 0.02, bool convex_only = true, double convexity_defect_tolerance = 0.05);
+	PolygonFinder(double error_tolerance = 0.02, bool convex_only = true);
 	virtual ~PolygonFinder();
 	std::vector<std::vector<cv::Point2i>> findPolygons(std::vector<std::vector<cv::Point2i>> contours);
 private:
--- code/apm/lib/src/PolygonFinder.cpp
+++ code/apm/lib/src/PolygonFinder.cpp
@@ -4,9 +4,9 @@ namespace automatic_package_measuring {
 
 PolygonFinder::PolygonFinder(double error_tolerance, bool convex_only,
 		double convexity_defect_tolerance) :
-		error_tolerance(error_tolerance), convex_only(convex_only), convexity_defect_tolerance(
-				convexity_defect_tolerance) {
+		error_tolerance(error_tolerance), convex_only(convex_only) {
 }
+
 
 PolygonFinder::~PolygonFinder() {
 }
@@ -20,26 +20,10 @@ std::vector<std::vector<cv::Point2i>> PolygonFinder::findPolygons(
 		double curve_length = cv::arcLength(contour, true);
 		cv::approxPolyDP(contour, polygon, curve_length * error_tolerance,
 				true);
-		if (!convex_only || isConvex(polygon, curve_length))
+		if (!convex_only || cv::isContourConvex(polygon))
 			polygons.push_back(polygon);
 	}
 	return polygons;
-}
-
-bool PolygonFinder::isConvex(std::vector<cv::Point2i> &polygon,
-		double curve_length) {
-	std::vector<int> hull;
-	cv::convexHull(polygon, hull);
-
-	if (polygon.size() < 4 || hull.size() < 4)
-			return false;
-	std::vector<cv::Vec4i> defects;
-	cv::convexityDefects(polygon, hull, defects);
-	double sum_defects = 0;
-	for (auto defect : defects)
-		sum_defects += defect[3];
-	return sum_defects / curve_length <= convexity_defect_tolerance;
-
 }
 
 } /* namespace automatic_package_measuring */
