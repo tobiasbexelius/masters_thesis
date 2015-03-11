d0a68ccfe62e2187d2aa03e472881bd15b6b6c51
Tobias Andersson <tobias.ason@gmail.com>
2015-03-11 13:37:23 +0100

    Add contour pruning

--- code/apm/lib/include/ContourExtractor.h
+++ code/apm/lib/include/ContourExtractor.h
@@ -7,12 +7,13 @@ namespace automatic_package_measuring {
 
 class ContourExtractor {
 public:
-	ContourExtractor(bool external_only = false);
+	ContourExtractor();
 	virtual ~ContourExtractor();
-	std::vector<std::vector<cv::Point2i>> extractContours(cv::Mat edges);
+	std::vector<std::vector<cv::Point>> extractContours(cv::Mat edges, bool external_only = false);
+	void pruneShortContours(std::vector<std::vector<cv::Point>>& contours, double min_length);
+	void prunePeripheralContours(std::vector<std::vector<cv::Point>>& contours, const cv::Mat& image, double placement_constraint);
 private:
-	bool external_only;
-
+	static bool isContourCentered(std::vector<cv::Point>& contour, double placement_constraint, int img_height, int img_width);
 };
 
 } /* namespace automatic_package_measuring */
--- code/apm/lib/src/ContourExtractor.cpp
+++ code/apm/lib/src/ContourExtractor.cpp
@@ -1,25 +1,85 @@
 #include "../include/ContourExtractor.h"
+#include <assert.h>
 
 namespace automatic_package_measuring {
 
-ContourExtractor::ContourExtractor(bool external_only): external_only(external_only) {
+ContourExtractor::ContourExtractor() {
 }
 
 ContourExtractor::~ContourExtractor() {
 }
 
-std::vector<std::vector<cv::Point2i>> ContourExtractor::extractContours(cv::Mat edges) {
+std::vector<std::vector<cv::Point>> ContourExtractor::extractContours(
+		cv::Mat edges, bool external_only) {
+
 	std::vector<std::vector<cv::Point2i>> contours;
 	std::vector<cv::Vec4i> hierarchy;
 
 	int mode;
-	if(external_only)
+	if (external_only)
 		mode = CV_RETR_EXTERNAL;
 	else
-		mode = CV_RETR_TREE;
-	cv::findContours(edges, contours, hierarchy, mode,
-				CV_CHAIN_APPROX_SIMPLE);
+		mode = CV_RETR_LIST;
+
+	cv::findContours(edges, contours, hierarchy, mode, CV_CHAIN_APPROX_SIMPLE);
+
 	return contours;
 }
 
+/**
+ * Removes contours shorter than min_length
+ */
+void ContourExtractor::pruneShortContours(
+		std::vector<std::vector<cv::Point> >& contours, double min_length) {
+
+	contours.erase(
+			std::remove_if(contours.begin(), contours.end(),
+					[min_length](std::vector<cv::Point>& contour) {
+						return cv::arcLength(contour, false) < min_length;}),
+			contours.end());
+}
+
+/**
+ * Removes contours which center is outside placement_constraint.
+ * placement_constraint is should be in range (0, 0.5) and decides
+ * how close to the center of the image, that the center of a contour
+ *  needs to be.
+ *
+ *  A value of 0.25 will prune contours which center is
+ *  outside the ranges (0.25, 0.75) times the image dimensions on either axis.
+ */
+void ContourExtractor::prunePeripheralContours(
+		std::vector<std::vector<cv::Point> >& contours, const cv::Mat& image,
+		double placement_constraint) {
+
+	int img_width = image.cols;
+	int img_height = image.rows;
+	contours.erase(
+			std::remove_if(contours.begin(), contours.end(),
+					[placement_constraint, img_height, img_width](std::vector<cv::Point>& contour) {
+						return !isContourCentered(contour, placement_constraint, img_height, img_width);}),
+			contours.end());
+}
+
+bool ContourExtractor::isContourCentered(std::vector<cv::Point>& contour,
+		double placement_constraint, int img_height, int img_width) {
+
+	assert(placement_constraint >= 0 && placement_constraint <= 1);
+
+	cv::Rect bounding_rect = cv::boundingRect(contour);
+
+	int center_x = bounding_rect.x + bounding_rect.width / 2;
+	int center_y = bounding_rect.y + bounding_rect.height / 2;
+
+	int min_x = img_width * placement_constraint;
+	int min_y = img_height * placement_constraint;
+
+	int max_x = img_width * (1 - placement_constraint);
+	int max_y = img_height * (1 - placement_constraint);
+
+	return center_x >= min_x && center_y >= min_y && center_x <= max_x
+			&& center_y <= max_y;
+}
+
 } /* namespace automatic_package_measuring */
+
